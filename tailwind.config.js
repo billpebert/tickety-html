9da9ff20dd17332bda784122153e9f9b794a8707
Billy Ponto <bpgponto@gmail.com>
2023-06-28 00:18:47 +0800

    landing page
    - extract assets & colors
    - slicing landing content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "50px",
+        xl: "100px",
+      },
+    },
+    extend: {
+      colors: {
+        "dark-indigo": "#0D0322",
+        primary: "#27164B",
+        secondary: "#82D0DF",
+        "butter-yellow": "#F7FF82",
+        "lavender-pink": "#DF82CB",
+        "iron-grey": "#DAD5E4",
+        "pastel-purple": "#A99FBD"
+      },
+    },
   },
   plugins: [],
 }
